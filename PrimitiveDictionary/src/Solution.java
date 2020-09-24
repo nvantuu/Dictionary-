16034ee229116f61138e680c9ddef0efaf37e122
nvantuu <vantul1720@gmail.com>
2020-09-24 23:04:00 +0700

    sua lai tong the lan 1

---
+++ PrimitiveDictionary/src/Solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+    // Main function.
+    public static void main(String[] args) {
+        DictionaryCommandline dic = new DictionaryCommandline();
+        dic.dictionaryBasic();
+    }
+}
--- PrimitiveDictionary/src/Word.java
+++ PrimitiveDictionary/src/Word.java
@@ -1,14 +1,39 @@
 public class Word {
-    private final String word_target;
-    private final String word_explain;
-    Word(String target, String explain) {
-        this.word_target = target;
-        this.word_explain = explain;
+    private  String word_target;
+    private  String word_explain;
+
+    public void setWord_target(String word_target) {
+        this.word_target = word_target;
     }
+
     public String getWord_target() {
         return word_target;
     }
+
+    public void setWord_explain(String word_explain) {
+        this.word_explain = word_explain;
+    }
+
     public String getWord_explain() {
         return word_explain;
     }
+    // Constructors (overloading)
+    /**
+     * Constructor has no parameter.
+     */
+    Word() { // 1st (default) constructor
+        this.word_target = null;
+        this.word_explain = null;
+    }
+
+    /**
+     * Constructor takes 2 parameters.
+     * @param word_target is 1 word, followed by it's Vietnamese meaning
+     * @param word_explain is Vietnamese meaning
+     */
+    Word(String word_target, String word_explain) {
+        this.word_target = word_target;
+        this.word_explain = word_explain;
+    }
+
 }
