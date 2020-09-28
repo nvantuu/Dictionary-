382c78017bc73cd577cac43606bd80429c2227eb
Bui Van Toan <buivantoanbg2001@gmail.com>
2020-09-28 12:36:13 +0700

    Dictionary ver 1.0

--- PrimitiveDictionary/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    // Main function.
-    public static void main(String[] args) {
-        DictionaryCommandline dic = new DictionaryCommandline();
-        dic.dictionaryBasic();
-    }
-}
