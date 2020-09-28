91aba8d769f9ad660c8b80cef544bfa4eda5aef9
Bui Van Toan <buivantoanbg2001@gmail.com>
2020-09-28 12:02:26 +0700

    Finished Dictionary Commandline ver 1.0

---
+++ Dictionary/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    /**
+     * The main function.
+     */
+    public static void main(String[] args) {
+        DictionaryCommandline dic = new DictionaryCommandline();
+        dic.dictionaryBasic();
+    }
+}
